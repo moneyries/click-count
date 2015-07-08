585b89a16f69283617d7350907cc7f7947827429
Brian Ries <bkries@gmail.com>
2015-07-07 20:32:46 -0400

    More changes

--- js/main.js
+++ js/main.js
@@ -11,10 +11,20 @@ $(function() {
   	console.log(count);
 
   	if (count == 5) {
-  		alert('hit 5')
+  		//alert('hit 5')
+  		$('body').css('background-color', 'green');
+  	} else if(count > 6) {
+  		$('body').css('background-color', 'blue');
+  	} else if(count == 6) {
+  		$('body').css('background-color', 'purple');
+  	} else {
+  		$('body').css('background-color', 'red');
   	}
 
-  	console.log('after if');
+  	if(count == 6) {
+  		 $('body').css('background-color', 'yellow');
+  	}
+
   }
 
 });
